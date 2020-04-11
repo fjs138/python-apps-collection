3746c6da049dbc226ae699414247f77c64526c31
Frank Santaguida <fjs138@gmail.com>
2020-04-11 01:04:51 -0400

    Add files via upload

---
+++ math.py
@@ -0,0 +1,13 @@
+int total
+int square(int x)
+{return x*x
+}
+int squareofsum(int x, int y)
+{
+int z = square(x+y)
+return z
+}
+int main()
+{int a = 4, b = 8
+total = squareofsum(a,b)
+print(output = %d, total)}
